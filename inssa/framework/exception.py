9faf3cf6ea3904039b86393ab3f50c3a9c725003
InssaGwajang <inssa.gwajang@gmail.com>
2022-06-20 22:57:39 +0900

    framework, exception

---
+++ inssa/framework/exception.py
@@ -0,0 +1,48 @@
+from typing import Final
+import sys, os
+
+from ..library import Trace, LOOP
+
+
+_TRACE: Final = Trace("exception", group="Framework")
+
+
+def exception(*, terminate: bool = False) -> None:
+    TRACE = _TRACE.CRITICAL
+
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    ts = []
+    while exc_traceback is not None:
+        ts.append(exc_traceback)
+        exc_traceback = exc_traceback.tb_next
+
+    stacks = [
+        [
+            str(t.tb_frame.f_code.co_name),
+            str(os.path.basename(t.tb_frame.f_code.co_filename)),
+            str(t.tb_lineno),
+        ]
+        for i, t in enumerate(reversed(ts))
+    ]
+    widths = (
+        max([len(stack[0]) for stack in stacks]) + 2,
+        max([len(stack[1]) for stack in stacks]),
+        max([len(stack[2]) for stack in stacks]),
+    )
+
+    TRACE(f"exception {exc_type.__name__} raised for {exc_value}")
+    TRACE(f"call stack:")
+    LOOP(
+        TRACE(
+            ("   >>" if not index else "     "),
+            (stack[0] + "()").ljust(widths[0]),
+            "in",
+            stack[1].ljust(widths[1]) + ",",
+            stack[2].ljust(widths[2]) + " line",
+            ("<<" if not index else ""),
+        )
+        for index, stack in enumerate(stacks)
+    )
+
+    if terminate:
+        sys.exit()
---
+++ tests/framework/test_exception.py
@@ -0,0 +1,35 @@
+from functools import reduce
+from unittest import TestCase
+import os, sys, pytest
+
+DEPTH: int = 2
+sys.path.append(
+    reduce(  # parent directory
+        lambda path, _: os.path.dirname(os.path.abspath(path)),
+        range(DEPTH),
+        os.path.abspath(os.path.dirname(__file__)),
+    )
+)
+
+from inssa.library import clear
+
+if __name__ == "__main__":
+    pytest.main([os.path.realpath(__file__).replace(os.getcwd(), "")[1:]])
+    clear()
+
+
+from inssa.framework import exception
+
+
+class TestInterval(TestCase):
+    def test_exception(self):
+        self.assertRaises(ValueError, exception)
+
+        def divide_zero() -> None:
+            try:
+                1 / 0
+
+            except Exception:
+                exception()
+
+        self.assertIsNone(divide_zero())
